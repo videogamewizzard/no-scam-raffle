6df4e46cec65f3c772e9ddc74780553c2b72f719
videogamewizzard <46911538+videogamewizzard@users.noreply.github.com>
2019-02-09 18:43:09 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const raffleArray = [];
-const flatArray = [];
-
 $(function() {
   $("#entries, #name").keyup(function() {
     if ($(this).val() == "") {
@@ -11,6 +8,9 @@ $(function() {
   });
 });
 
+const raffleArray = [];
+const flatArray = [];
+
 const randomize = array => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -19,46 +19,7 @@ const randomize = array => {
   return array;
 };
 
-$("#submit").on("click", event => {
-  event.preventDefault();
-  let name = $("#donator")
-    .val()
-    .trim();
-  let entries = $("#entries")
-    .val()
-    .trim();
-  let newName = `${name} `;
-  let repeatedName = newName.repeat(entries);
-  let trimmedName = repeatedName.trim();
-  let fullEntry = trimmedName.split(" ");
-  console.log(fullEntry);
-  raffleArray.push(fullEntry);
-  $("#donator").val("");
-  $("#entries").val("");
-  const flatArray = raffleArray.flat(1);
-  let randomizedArray = randomize(flatArray);
-  console.log(randomizedArray);
-  let totalEntries = randomizedArray.reduce(function(obj, item) {
-    obj[item] = (obj[item] || 0) + 1;
-    return obj;
-  }, {});
-  $("#odds").text(`Entries: ${JSON.stringify(totalEntries)}`);
-  $("#donation-total").text(`Total Entries: ${flatArray.length}`);
-  randomizeTimer();
-  $("#pick-winner").prop("disabled", false);
-});
-
-$("#pick-winner").on("click", event => {
-  event.preventDefault();
-  let flatArray = raffleArray.flat(1);
-  const random = randomize(flatArray);
-  console.log(random);
-  let winner = random[Math.floor(Math.random() * random.length)];
-  console.log(winner);
-  $("#winner").text(`THE WINNER IS... ${winner}`);
-});
-
-function randomizeTimer() {
+const randomizeTimer = () => {
   let current_progress = 0;
   let interval = setInterval(function() {
     current_progress += getRandomInt(20, 50);
@@ -74,8 +35,51 @@ function randomizeTimer() {
         .removeClass("progress-bar-animated");
     }
   }, 1000);
-}
+};
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+
+const doSubmit = () => {
+  let name = $("#donator")
+    .val()
+    .trim();
+  let entries = $("#entries")
+    .val()
+    .trim();
+  let newName = `${name} `;
+  let repeatedName = newName.repeat(entries);
+  let trimmedName = repeatedName.trim();
+  let fullEntry = trimmedName.split(" ");
+  raffleArray.push(fullEntry);
+  $("#donator").val("");
+  $("#entries").val("");
+  const flatArray = raffleArray.flat(1);
+  let randomizedArray = randomize(flatArray);
+  let totalEntries = randomizedArray.reduce(function(obj, item) {
+    obj[item] = (obj[item] || 0) + 1;
+    return obj;
+  }, {});
+  $("#odds").text(`Entries: ${JSON.stringify(totalEntries)}`);
+  $("#donation-total").text(`Total Entries: ${flatArray.length}`);
+  randomizeTimer();
+  $("#pick-winner").prop("disabled", false);
+};
+
+const pickWinner = () => {
+  let flatArray = raffleArray.flat(1);
+  const random = randomize(flatArray);
+  let winner = random[Math.floor(Math.random() * random.length)];
+  $("#winner").text(`THE WINNER IS... ${winner}`);
+};
+
+$("#submit").on("click", event => {
+  event.preventDefault();
+  doSubmit();
+});
+
+$("#pick-winner").on("click", event => {
+  event.preventDefault();
+  pickWinner();
+});
